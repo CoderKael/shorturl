2d1553d5d69b981c5b4464250afaf0f10d8bd0ae
Mikael Ramdeen <mikael.ramdeen@hotmail.com>
2022-10-08 00:45:57 -0400

    first

--- index.js
+++ index.js
@@ -14,10 +14,48 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const originalUrls = [];
+const shortUrls = [];
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+  const foundIndex = originalUrls.IndexOf(url);
+
+  if(!url.includes("https://") && !url.includes("http://")) {
+    return res.json({
+      error: "invalid url"
+    });
+  }
+
+  if(foundIndex < 0) {
+    originalUrls.push(url);
+    shortUrls.push(shortUrls.length);
+  }
+
+  return res.json({
+    original_url: url,
+    short_url: shortUrls.length - 1
+  });
+
+  return res.json({
+    original_url: url,
+    short_url: shortUrls[foundIndex]
+  });
 });
+
+app.get('/api/shorturl/:shorturl', (req, res) => {
+  const shorturl = parseInt(req.param.shorturl);
+  const foundIndex = shortUrl.IndexOf(shorturl);
+
+  if (foundIndex < 0) {
+    return res.json({
+      "error": "No shorturl for given input"
+    });
+  }
+
+  res.redirect(originalUrls[foundIndex]);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
